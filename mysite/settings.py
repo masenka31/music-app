b2d389359d25e054108e380570d2c8145ab65813
masenka31 <masenka.maskova@gmail.com>
2020-11-12 12:11:28 +0100

    settings2

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 SESSION_ENGINE= 'django.contrib.sessions.backends.file'
 SESSION_SAVE_EVERY_REQUEST = True
